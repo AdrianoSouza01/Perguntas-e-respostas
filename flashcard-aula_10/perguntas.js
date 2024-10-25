46e85fd52f70b69be10c9603588f13adc9d9dfbe
AdrianoSouza01 <00001108515137sp@al.educacao.sp.gov.br>
2024-10-25 07:32:45 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Games',
+    'Qual é o mehor jogo de pc?',
+    'É o League of Legends'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Mosocou'
+)
+
+criaCartao(
+    'Química',
+    'Qual o metal mais denso?',
+    'O metal mais denso é o ósmio'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como escreve (Me chama de lord) em inglês',
+    'Me he is LORD HE HE'
+)
